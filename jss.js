07e54c56050e440c03b7abb18d669389a98e01e8
COR Coding <corcoding@coding-14.attlocal.net>
2023-08-12 22:14:17 -0400

    changing GenCatBreed to a function that generates a button in breedListBox for each cat breed from the Api.

--- jss.js
+++ jss.js
@@ -1,17 +1,35 @@
-const GenCatFacts = ()=> {
+const GenCatFacts = () => {
     fetch("https://catfact.ninja/fact")
-    .then(response => response.json())
-    .then(result=> result.fact)
-    .then(fact => document.getElementById("fact").innerText = fact
-    );
+        .then(response => response.json())
+        .then(result => result.fact)
+        .then(fact => document.getElementById("fact").innerText = fact
+        );
 }
 
-const GenCatBreed = ()=> {
-  fetch("https://api.thecatapi.com/v1/breeds")
-  .then (response => response.json())
-  .then (data => {
-    console.log(data);
-  })
+function GenCatBreed(){
+    fetch("https://api.thecatapi.com/v1/breeds")
+        .then(response => {
+            return response.json()
+        })
+        .then(catBreed => {
+            console.log(catBreed)
+            for(let i = 0; i < 20; i++){
+                console.log(catBreed[i].name)
+                let breed = document.createElement('button');
+                let breedList = document.getElementById('breedListBox');
+                
+                breed.classList.add('breedBtn');
+                breed.innerText = catBreed[i].name;
+                
+                breedList.appendChild(breed);
+            }        
+            
+        })
 }
 
 GenCatBreed()
+
+// name, description, temperment, energy_level, social_needs, health_issues, vocalization, 
+// function genCatBreedInfo(breedName)
+
+
